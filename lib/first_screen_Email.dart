3eeef1b3d3a447ba4466b0ce91f10a6c21d863d9
DioDarrell <rekudiodarrell@gmail.com>
2021-04-23 14:39:16 +0700

    Perbaikan dan penambahan Login email & pass

---
+++ lib/first_screen_Email.dart
@@ -0,0 +1,76 @@
+// Copyright (c) 2019 Souvik Biswas
+// Copyright (c) 2019 Souvik Biswas
+import 'package:flutter/material.dart';
+import 'package:flutter_firebase/login_page.dart';
+import 'package:flutter_firebase/sign_in.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+class FirstScreenEmail extends StatelessWidget {
+  final auth = FirebaseAuth.instance;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topRight,
+            end: Alignment.bottomLeft,
+            colors: [Colors.blue[100], Colors.blue[400]],
+          ),
+        ),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            mainAxisSize: MainAxisSize.max,
+            children: <Widget>[
+              Text(
+                "You're logged in ! Hore !",
+                style: TextStyle(
+                    fontSize: 25,
+                    color: Colors.deepPurple,
+                    fontWeight: FontWeight.bold),
+              ),
+              SizedBox(height: 20),
+              Text(
+                'EMAIL',
+                style: TextStyle(
+                    fontSize: 15,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.black54),
+              ),
+              Text(
+                auth.currentUser.email,
+                style: TextStyle(
+                    fontSize: 25,
+                    color: Colors.deepPurple,
+                    fontWeight: FontWeight.bold),
+              ),
+              SizedBox(height: 40),
+              RaisedButton(
+                onPressed: () {
+                  signOutGoogle();
+
+                  Navigator.of(context).pushAndRemoveUntil(
+                      MaterialPageRoute(builder: (context) {
+                    return LoginPage();
+                  }), ModalRoute.withName('/'));
+                },
+                color: Colors.deepPurple,
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Text(
+                    'Sign Out',
+                    style: TextStyle(fontSize: 25, color: Colors.white),
+                  ),
+                ),
+                elevation: 5,
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(40)),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
--- lib/login_page.dart
+++ lib/login_page.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_firebase/sign_in.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter_firebase/first_screen.dart';
+import 'package:flutter_firebase/first_screen_Email.dart';
 
 class LoginPage extends StatefulWidget {
   @override
@@ -8,6 +10,9 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
+  String _email, _password;
+  final auth = FirebaseAuth.instance;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -19,6 +24,58 @@ class _LoginPageState extends State<LoginPage> {
             mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
               FlutterLogo(size: 150),
+              SizedBox(height: 50),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: TextField(
+                  keyboardType: TextInputType.emailAddress,
+                  decoration: InputDecoration(hintText: 'Email'),
+                  onChanged: (value) {
+                    setState(() {
+                      _email = value.trim();
+                    });
+                  },
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: TextField(
+                  obscureText: true,
+                  decoration: InputDecoration(hintText: 'Password'),
+                  onChanged: (value) {
+                    setState(() {
+                      _password = value.trim();
+                    });
+                  },
+                ),
+              ),
+              Row(mainAxisAlignment: MainAxisAlignment.spaceAround, children: [
+                RaisedButton(
+                    color: Theme.of(context).accentColor,
+                    child: Text('Login'),
+                    onPressed: () {
+                      auth
+                          .signInWithEmailAndPassword(
+                              email: _email, password: _password)
+                          .then((_) {
+                        Navigator.of(context).pushReplacement(MaterialPageRoute(
+                            builder: (context) => FirstScreenEmail()));
+                      });
+                    }),
+                RaisedButton(
+                  color: Theme.of(context).accentColor,
+                  child: Text('Sign up'),
+                  onPressed: () {
+                    auth
+                        .createUserWithEmailAndPassword(
+                            email: _email, password: _password)
+                        .then((_) {
+                      Navigator.of(context).pushReplacement(MaterialPageRoute(
+                          builder: (context) => FirstScreenEmail()));
+                    });
+                  },
+                )
+              ]),
               SizedBox(height: 50),
               _signInButton(),
             ],
@@ -36,7 +93,7 @@ class _LoginPageState extends State<LoginPage> {
           if (result != null) {
             Navigator.of(context).push(
               MaterialPageRoute(
-                builder: (context) {
+                builder: (context)  {
                   return FirstScreen();
                 },
               ),
@@ -53,7 +110,9 @@ class _LoginPageState extends State<LoginPage> {
           mainAxisSize: MainAxisSize.min,
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            Image(image: AssetImage("assets/google_logo.png"), height: 35.0),
+            Image.network('https://img.icons8.com/color/452/google-logo.png',
+                height: 35.0),
+            //Image(image: AssetImage("assets/google_logo.png"), height: 35.0),
             Padding(
               padding: const EdgeInsets.only(left: 10),
               child: Text(
